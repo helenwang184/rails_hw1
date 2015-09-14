6886e124fd31bc57923e291ff15409aaec429318
helenwang184 <helen.wang184@berkeley.edu>
2015-09-14 01:14:15 -0700

    submit now

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(array)
+  	array = array.sort.uniq.map { |s| s.to_i }
+  	array = array.map { |s| s+=2 }
+  	array = array.delete_if {|x| x>10 || x%2==1}
+  	sum = 0
+  	return array.inject{|sum,x| sum+x }
+  end
 end
